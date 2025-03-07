bd998141fe601c2eb38471b55afcfaf918c975f1
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-03-07 05:35:21 +0100

    miscellaneous

--- src/pages/MainPages/Homepage.tsx
+++ src/pages/MainPages/Homepage.tsx
@@ -1,10 +1,14 @@
 import { Button, Typography } from "@mui/material";
+import { Link } from "react-router";
 
 function Homepage() {
   return (
     <div>
       <Button>Click me!</Button>
       <Typography>Dashboard Homepage</Typography>
+      <Button>
+        <Link to="/goods">Goods page</Link>
+      </Button>
     </div>
   );
 }
