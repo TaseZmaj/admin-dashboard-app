f1ee5c1db5a5d143edb7ab9b691f8f9ec6b7426d
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-03-09 23:54:45 +0100

    Added 2 logos in assets

--- src/pages/MainPages/Homepage.tsx
+++ src/pages/MainPages/Homepage.tsx
@@ -1,14 +1,9 @@
-import { Button, Typography } from "@mui/material";
-import { Link } from "react-router";
+import { Typography } from "@mui/material";
 
 function Homepage() {
   return (
     <div>
-      <Button>Click me!</Button>
       <Typography>Dashboard Homepage</Typography>
-      <Button>
-        <Link to="/goods">Goods page</Link>
-      </Button>
     </div>
   );
 }
