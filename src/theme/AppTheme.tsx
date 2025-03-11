d8d202e162c67e0b8248cc604d9c58ef93c0cbe4
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-03-11 17:45:54 +0100

    added a dark theme

--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -2,8 +2,9 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { orange, blue, red } from "@mui/material/colors";
 
 export default function AppTheme({ children }: { children: React.ReactNode }) {
-  const customTheme = createTheme({
+  const lightMode = createTheme({
     palette: {
+      mode: "light",
       primary: {
         main: orange[400], //#ffca28
         light: orange[200],
@@ -22,5 +23,23 @@ export default function AppTheme({ children }: { children: React.ReactNode }) {
       fontFamily: ["Inter", "Roboto"].join(","),
     },
   });
-  return <ThemeProvider theme={customTheme}>{children}</ThemeProvider>;
+  const darkMode = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: orange[400],
+        light: orange[200],
+        dark: orange[600],
+      },
+      secondary: {
+        main: blue[400],
+        light: blue[200],
+        dark: blue[700],
+      },
+      warning: {
+        main: red[600],
+      },
+    },
+  });
+  return <ThemeProvider theme={darkMode}>{children}</ThemeProvider>;
 }
