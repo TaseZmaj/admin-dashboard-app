b14224103fb634226a03d33492360b509e66130b
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-03-11 17:47:59 +0100

    Adjusted login page

---
+++ src/pages/PublicPages/Login.tsx
@@ -0,0 +1,21 @@
+import { Paper } from "@mui/material";
+import LoginModal from "../../components/Dynamic/Login/LoginModal.tsx";
+import ThemeToggle from "../../components/Dynamic/ThemeToggle.tsx";
+
+export default function Login() {
+  return (
+    <Paper
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 0,
+      }}
+    >
+      <ThemeToggle />
+      <LoginModal />
+    </Paper>
+  );
+}
