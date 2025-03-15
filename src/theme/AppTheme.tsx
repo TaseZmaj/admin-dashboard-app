fb84f3b989bf0e7bda6821081e1e3f083e348a8d
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-03-15 02:40:14 +0100

    changed the logic so that it uses colorSchemes

--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -2,44 +2,88 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { orange, blue, red } from "@mui/material/colors";
 
 export default function AppTheme({ children }: { children: React.ReactNode }) {
-  const lightMode = createTheme({
-    palette: {
-      mode: "light",
-      primary: {
-        main: orange[400], //#ffca28
-        light: orange[200],
-        dark: orange[600],
+  const customTheme = createTheme({
+    colorSchemes: {
+      light: {
+        palette: {
+          mode: "light",
+          primary: {
+            main: orange[400], //#ffca28
+            light: orange[200],
+            dark: orange[600],
+          },
+          secondary: {
+            main: blue[400],
+            light: blue[200],
+            dark: blue[700],
+          },
+          warning: {
+            main: red[600],
+          },
+        },
       },
-      secondary: {
-        main: blue[400],
-        light: blue[200],
-        dark: blue[700],
-      },
-      warning: {
-        main: red[600],
+      dark: {
+        palette: {
+          mode: "dark",
+          primary: {
+            main: orange[400],
+            light: orange[200],
+            dark: orange[600],
+          },
+          secondary: {
+            main: blue[400],
+            light: blue[200],
+            dark: blue[700],
+          },
+          warning: {
+            main: red[600],
+          },
+        },
       },
     },
-    typography: {
-      fontFamily: ["Inter", "Roboto"].join(","),
+    cssVariables: {
+      colorSchemeSelector: "class",
     },
   });
-  const darkMode = createTheme({
-    palette: {
-      mode: "dark",
-      primary: {
-        main: orange[400],
-        light: orange[200],
-        dark: orange[600],
-      },
-      secondary: {
-        main: blue[400],
-        light: blue[200],
-        dark: blue[700],
-      },
-      warning: {
-        main: red[600],
-      },
-    },
-  });
-  return <ThemeProvider theme={darkMode}>{children}</ThemeProvider>;
+
+  // const lightMode = createTheme({
+  //   palette: {
+  //     mode: "light",
+  //     primary: {
+  //       main: orange[400], //#ffca28
+  //       light: orange[200],
+  //       dark: orange[600],
+  //     },
+  //     secondary: {
+  //       main: blue[400],
+  //       light: blue[200],
+  //       dark: blue[700],
+  //     },
+  //     warning: {
+  //       main: red[600],
+  //     },
+  //   },
+  //   typography: {
+  //     fontFamily: ["Inter", "Roboto"].join(","),
+  //   },
+  // });
+  // const darkMode = createTheme({
+  //   palette: {
+  //     mode: "dark",
+  //     primary: {
+  //       main: orange[400],
+  //       light: orange[200],
+  //       dark: orange[600],
+  //     },
+  //     secondary: {
+  //       main: blue[400],
+  //       light: blue[200],
+  //       dark: blue[700],
+  //     },
+  //     warning: {
+  //       main: red[600],
+  //     },
+  //   },
+  // });
+  return <ThemeProvider theme={customTheme}>{children}</ThemeProvider>;
 }
