f41ed4062a6ff319a60ffb17fefb4200ae7270dc
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-03-15 02:39:28 +0100

    Prepping layout for the main Dashboard stuff

--- src/pages/UtilPages/Layout.tsx
+++ src/pages/UtilPages/Layout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router";
 import SideBarNav from "../../components/Dynamic/Navigation/SideBarNav";
-import { Paper } from "@mui/material";
+import { Paper, Box } from "@mui/material";
 
 export default function Layout() {
   return (
-    <Paper sx={{ display: "flex", borderRadius: 0 }}>
+    <Paper
+      sx={{ display: "flex", borderRadius: 0, width: "100%", height: "100%" }}
+    >
       <SideBarNav />
-      <Outlet />
+      <Box component="main" sx={{ flexGrow: 1 }}>
+        <Outlet />
+      </Box>
     </Paper>
   );
 }
