e8bde57a124e319de6114f83959e6c4262836d8f
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-03-21 03:33:06 +0100

    Made the background color darker and added a few lines that I will be useful later

--- src/pages/UtilPages/Layout.tsx
+++ src/pages/UtilPages/Layout.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router";
 import SideBarNav from "../../components/Dynamic/Navigation/SideBarNav";
 import { Paper, Box } from "@mui/material";
+import { Theme, useTheme } from "@mui/material";
 
 export default function Layout() {
+  const { palette } = useTheme() as Theme;
   return (
     <Paper
-      sx={{ display: "flex", borderRadius: 0, width: "100%", height: "100%" }}
+      sx={{
+        display: "flex",
+        borderRadius: 0,
+        width: "100%",
+        height: "100%",
+        backgroundColor: palette.background.default,
+      }}
     >
       <SideBarNav />
       <Box component="main" sx={{ flexGrow: 1 }}>
--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -38,52 +38,39 @@ export default function AppTheme({ children }: { children: React.ReactNode }) {
           warning: {
             main: red[600],
           },
+          background: {
+            default: "#000000",
+          },
         },
       },
     },
-    cssVariables: {
-      colorSchemeSelector: "class",
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 960,
+        lg: 1280,
+        xl: 1920,
+      },
     },
+    //TODO: Make components like this one and separate them in different files
+    // components: {
+    //   MuiPaper: {
+    //     styleOverrides: {
+    //       root: {
+    //         boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)", // Custom shadow
+    //         // Optionally, you can specify shadows for different elevations
+    //         "&.MuiPaper-elevation1": {
+    //           boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)", // Light shadow for elevation 1
+    //         },
+    //         "&.MuiPaper-elevation2": {
+    //           boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.15)", // Slightly stronger shadow for elevation 2
+    //         },
+    //         // You can add more elevation levels as needed
+    //       },
+    //     },
+    //   },
+    // },
   });
-
-  // const lightMode = createTheme({
-  //   palette: {
-  //     mode: "light",
-  //     primary: {
-  //       main: orange[400], //#ffca28
-  //       light: orange[200],
-  //       dark: orange[600],
-  //     },
-  //     secondary: {
-  //       main: blue[400],
-  //       light: blue[200],
-  //       dark: blue[700],
-  //     },
-  //     warning: {
-  //       main: red[600],
-  //     },
-  //   },
-  //   typography: {
-  //     fontFamily: ["Inter", "Roboto"].join(","),
-  //   },
-  // });
-  // const darkMode = createTheme({
-  //   palette: {
-  //     mode: "dark",
-  //     primary: {
-  //       main: orange[400],
-  //       light: orange[200],
-  //       dark: orange[600],
-  //     },
-  //     secondary: {
-  //       main: blue[400],
-  //       light: blue[200],
-  //       dark: blue[700],
-  //     },
-  //     warning: {
-  //       main: red[600],
-  //     },
-  //   },
-  // });
   return <ThemeProvider theme={customTheme}>{children}</ThemeProvider>;
 }
